33aecc4398dcbec1be9381baf4f7134b9e93b2c5
Guilherme Nascimento Cafure <GNC@bdmg.mg.gov.br>
2022-09-28 04:07:20 -0400

    revert "h2 security config"
    This reverts commit a0118a57f577fb0c25bcac9ccb2b9c00f820d2a0.

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,10 +1,7 @@
 package com.devsuperior.dscatalog.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -17,18 +14,13 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
-	private Environment env;
-	
-	@Autowired
 	private JwtTokenStore tokenStore;
 
-	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+	private static final String[] PUBLIC = { "/oauth/token" };
 
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 
 	private static final String[] ADMIN = { "/users/**" };
-	
-	
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -37,12 +29,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		
-		//Console do h2 liberado
-		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
-			http.headers().frameOptions().disable();
-		}
-		
 		http.authorizeRequests().antMatchers(PUBLIC).permitAll()
 		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
 		.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
